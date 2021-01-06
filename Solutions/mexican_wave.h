54dc8ddc13a16c09a3c09bf553dba0255cb76ad0
n1k0m1 <erika.n1k0m1@gmail.com>
2021-01-06 14:07:12 +0200

    Create mexican_wave.h

---
+++ Solutions/mexican_wave.h
@@ -0,0 +1,16 @@
+#ifndef MEXICAN_WAVE_KATA
+#define MEXICAN_WAVE_KATA
+
+//https://www.codewars.com/kata/58f5c63f1e26ecda7e000029
+
+#include <vector>
+#include <string>
+
+std::vector<std::string> wave(std::string y)
+{
+	std::vector<std::string> mWave;
+
+	return mWave;
+}
+
+#endif // !MEXICAN_WAVE_KATA
