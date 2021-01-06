645c2a963dbf7e59f3e00ac63c9a940d2bedd0bc
n1k0m1 <erika.n1k0m1@gmail.com>
2021-01-06 10:24:48 +0200

    Update is_this_a_triangle.h
    Add link to kata

--- Solutions/is_this_a_triangle.h
+++ Solutions/is_this_a_triangle.h
@@ -1,6 +1,8 @@
 #ifndef IS_THIS_A_TRIANGLE_KATA
 #define IS_THIS_A_TRIANGLE_KATA
 
+//https://www.codewars.com/kata/56606694ec01347ce800001b
+
 namespace Triangle
 {
 	bool isTriangle(int a, int b, int c)
