9140e99fa0835588b420609e366ead8b7101c61f
n1k0m1 <erika.n1k0m1@gmail.com>
2021-01-06 09:56:26 +0200

    Update number_of_people_in_the_bus.h

--- Solutions/number_of_people_in_the_bus.h
+++ Solutions/number_of_people_in_the_bus.h
@@ -8,7 +8,12 @@
 
 unsigned int number(const std::vector<std::pair<int, int>>& busStops)
 {
+	int people_Count = 0;
 
+	for (std::pair<int, int> pair : busStops)
+		people_Count += pair.first - pair.second;
+
+	return people_Count;
 }
 
 #endif // !NUMBER_OF_PEOPLE_IN_THE_BUS_KATA
