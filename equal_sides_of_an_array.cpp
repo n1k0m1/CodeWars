1d47b35c72d8b1a290b655258cad01fef4042b9e
n1k0m1 <erika.n1k0m1@gmail.com>
2021-01-06 05:24:54 +0200

    Create equal_sides_of_an_array.cpp
    Create equal_sides_of_an_array.cpp file

---
+++ equal_sides_of_an_array.cpp
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/5679aa472b8f57fb8c000047
+
+#include <vector>
+using namespace std;
+
+int find_even_index(const vector <int> numbers)
+{
+	int index = numbers.size();
+	return -1;
+}
