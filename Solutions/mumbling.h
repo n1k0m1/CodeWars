9e3659aca260747ce5722413db121d2ba69ac80c
n1k0m1 <erika.n1k0m1@gmail.com>
2021-01-06 10:27:46 +0200

    Create mumbling.h

---
+++ Solutions/mumbling.h
@@ -0,0 +1,14 @@
+#ifndef MUMBLING_KATA
+#define MUMBLING_KATA
+
+//https://www.codewars.com/kata/5667e8f4e3f572a8f2000039
+
+#include <string>
+
+class Accumul
+{
+public:
+    static std::string accum(const std::string& s);
+};
+
+#endif // !MUMBLING_KATA
