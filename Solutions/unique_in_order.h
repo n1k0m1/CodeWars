bac2a82e190b192d88493b053642fa979473c3c5
n1k0m1 <erika.n1k0m1@gmail.com>
2021-01-06 17:18:46 +0200

    Create unique_in_order.h

---
+++ Solutions/unique_in_order.h
@@ -0,0 +1,18 @@
+#ifndef UNIQUE_IN_ORDER_KATA
+#define UNIQUE_IN_ORDER_KATA
+
+//https://www.codewars.com/kata/54e6533c92449cc251001667
+
+#include <string>
+#include <vector>
+
+template <typename T> std::vector<T> uniqueInOrder(const std::vector<T>& iterable)
+{
+
+}
+std::vector<char> uniqueInOrder(const std::string& iterable)
+{
+
+}
+
+#endif
