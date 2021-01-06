0d5bd82331c13e2f896cfdfef9b5c3f889236234
n1k0m1 <erika.n1k0m1@gmail.com>
2021-01-06 09:42:05 +0200

    Create number_of_people_in_the_bus.h

---
+++ Solutions/number_of_people_in_the_bus.h
@@ -0,0 +1,15 @@
+#ifndef NUMBER_OF_PEOPLE_IN_THE_BUS_KATA
+#define NUMBER_OF_PEOPLE_IN_THE_BUS_KATA
+
+//https://www.codewars.com/kata/5648b12ce68d9daa6b000099
+
+#include <utility>
+#include <vector>
+
+unsigned int number(const std::vector<std::pair<int, int>>& busStops)
+{
+
+}
+
+#endif // !NUMBER_OF_PEOPLE_IN_THE_BUS_KATA
+
