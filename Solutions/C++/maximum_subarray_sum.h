aee52a3ef4fd6dde96960aa41d3b00bb0acbf0b5
n1k0m1 <erika.n1k0m1@gmail.com>
2021-01-08 04:56:42 +0200

    Create maximum_subarray_sum.h

---
+++ Solutions/C++/maximum_subarray_sum.h
@@ -0,0 +1,13 @@
+#ifndef MAXIMUM_SUBARRAY_SUM_KATA
+#define MAXIMUM_SUBARRAY_SUM_KATA
+
+//https://www.codewars.com/kata/54521e9ec8e60bc4de000d6c
+
+#include <vector>
+
+int maxSequence(const std::vector<int>& arr)
+{
+
+}
+
+#endif // !MAXIMUM_SUBARRAY_SUM_KATA
