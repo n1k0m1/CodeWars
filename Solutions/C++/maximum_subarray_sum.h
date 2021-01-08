bb18d520a55fb4ab8056987dae3e7313b749a352
n1k0m1 <erika.n1k0m1@gmail.com>
2021-01-08 07:42:04 +0200

    Update maximum_subarray_sum.h

--- Solutions/C++/maximum_subarray_sum.h
+++ Solutions/C++/maximum_subarray_sum.h
@@ -4,10 +4,35 @@
 //https://www.codewars.com/kata/54521e9ec8e60bc4de000d6c
 
 #include <vector>
+#include <algorithm>
 
 int maxSequence(const std::vector<int>& arr)
 {
+	int sum = 0;
+	
+	if (std::any_of(arr.cbegin(), arr.cend(), [](int x) {return x >= 0;}))
+	{
+		int tempSum = 0;
 
+		for (size_t carriage = arr.size(); carriage > 0; --carriage)
+		{
+			for (size_t carrStart = 0; carrStart + carriage <= arr.size(); ++carrStart)
+			{
+				for (size_t index = carrStart; index < carrStart + carriage; ++index)
+				{
+					tempSum += arr[index];
+				}
+
+				if (tempSum > sum)
+				{
+					sum = tempSum;
+				}
+				tempSum = 0;
+			}
+		}
+	}
+	
+	return sum;
 }
 
 #endif // !MAXIMUM_SUBARRAY_SUM_KATA
